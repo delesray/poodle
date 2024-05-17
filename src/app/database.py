89acfef533f7e5c30052ce48f94f2db27fc74d3a
DenitsaTH <denizza.cristova@gmail.com>
2024-05-17 18:04:21 +0300

    add mariadb url example

--- src/app/database.py
+++ src/app/database.py
@@ -1,9 +1,12 @@
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:pass@some_mariadb/dbname?charset=utf8mb4"
+# MARIADB EXAMPLE: "mysql+pymysql://scott:tiger@localhost/test?charset=utf8mb4"
 
 # factory that can create new database connections
 engine = create_engine(
@@ -12,4 +15,7 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = sqlalchemy.orm.declarative_base()  # The declarative_base() function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 2.0)
+
+# to create all tables:
+# Base.metadata.create_all(engine)
