87cd5b2ca8add097d2dd475368dd18ded967a269
DenitsaTH <denizza.cristova@gmail.com>
2024-05-19 17:23:08 +0300

    update db url

--- src/app/database/database.py
+++ src/app/database/database.py
@@ -2,18 +2,19 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from hidden import USER, PASS
 #from src.app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db" # connect_args={"check_same_thread": False}
 #SQLALCHEMY_DATABASE_URL = settings.DB_URL
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:pass@some_mariadb/dbname?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USER}:{PASS}@localhost/poodle?charset=utf8mb4"
 # MARIADB EXAMPLE: "mysql+pymysql://scott:tiger@localhost/test?charset=utf8mb4"
 
 # factory that can create new database connections
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )   # argument is needed only for SQLite. It's not needed for other databases.
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
