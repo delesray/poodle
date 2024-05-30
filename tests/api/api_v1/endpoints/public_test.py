8e1df05e576a6e64164bd93e5a62c716a42c830d
DenitsaTH <denizza.cristova@gmail.com>
2024-05-30 18:30:34 +0300

    public tests: fix test

--- tests/api/api_v1/endpoints/public_test.py
+++ tests/api/api_v1/endpoints/public_test.py
@@ -12,21 +12,7 @@ user = Account(email='dummy@mail.com',
 token = Token(access_token='valid_token', token_type='bearer')
 
 
-# def test_login_returns_token_when_correct_credentials(client: TestClient, mocker):
-#     mocker.patch('api.api_v1.routes.public.crud_user.try_login', return_value=user)
-#     mocker.patch('app.api.api_v1.routes.public.create_access_token', return_value=token)
-#
-#     login_data = {
-#         'username': 'dummy@mail.com',
-#         'password': 'dummypass'
-#     }
-#
-#     response = client.post('/login', data=login_data)
-#     assert response.status_code == 200
-#     assert response.json() == token.dict()
-
-@pytest.mark.asyncio
-async def test_login_returns_token_when_correct_credentials(client: TestClient, mocker):
+def test_login_returns_token_when_correct_credentials(client: TestClient, mocker):
     mocker.patch('api.api_v1.routes.public.crud_user.try_login', return_value=user)
     mocker.patch('api.api_v1.routes.public.create_access_token', return_value=token)
 
@@ -35,6 +21,6 @@ async def test_login_returns_token_when_correct_credentials(client: TestClient, 
         'password': 'dummypass'
     }
 
-    actual = await login(form_data=login_data, db=None)
-    assert actual == token
-
+    response = client.post('/login', data=login_data)
+    assert response.status_code == 200
+    assert response.json() == token.dict()
