d5ff51f7be47cb9730f88011230bff400e73a6c2
DenitsaTH <denizza.cristova@gmail.com>
2024-06-04 20:57:16 +0300

    admins: move dep override to conftest

--- src/app/tests/conftest.py
+++ src/app/tests/conftest.py
@@ -2,9 +2,10 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import Session, sessionmaker
-from core.oauth import get_student_required
+from core.oauth import get_student_required, get_admin_required
 from db.database import get_db, Base
 from main import app
+from tests.api.api_v1.endpoints.admins_test import dummy_admin
 from tests.api.api_v1.endpoints.student_test import dummy_student
 
 
@@ -63,3 +64,4 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_student_required] = lambda: dummy_student
+app.dependency_overrides[get_admin_required] = lambda: dummy_admin
