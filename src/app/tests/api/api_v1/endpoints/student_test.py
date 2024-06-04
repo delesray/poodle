20b6a9d5c046aff70259151697e8bf4f477cdad1
DenitsaTH <denizza.cristova@gmail.com>
2024-06-04 22:28:50 +0300

    students tests: test not authenticated, test upload picture

--- src/app/tests/api/api_v1/endpoints/student_test.py
+++ src/app/tests/api/api_v1/endpoints/student_test.py
@@ -1,8 +1,11 @@
 import io
+import pytest
 from fastapi.testclient import TestClient
+from core.oauth import get_student_required
 from db.models import Account, Student
 from core.security import Token
 from fastapi import status
+from main import app
 
 dummy_account = Account(account_id=1,
                         email='dummy@mail.com',
@@ -22,7 +25,7 @@ new_student_request = {
     "last_name": "dummyName"
 }
 
-new_student_response = {
+student_response = {
     "first_name": "dummyName",
     "last_name": "dummyName",
     "is_premium": False
@@ -30,6 +33,28 @@ new_student_response = {
 
 token = Token(access_token='valid_token',
               token_type='bearer')
+
+
+@pytest.mark.asyncio
+async def test_student_not_authenticated(client: TestClient):
+
+    # store the current dependency override
+    original_override = app.dependency_overrides.get(get_student_required)
+
+    # delete it
+    if get_student_required in app.dependency_overrides:
+        del app.dependency_overrides[get_student_required]
+
+    # execute the test with the original dep
+    response = client.get('/students')
+    data = response.json()
+
+    assert response.status_code == 401
+    assert data['detail'] == 'Not authenticated'
+
+    # restore the dependency override for the rest of the tests
+    if original_override:
+        app.dependency_overrides[get_student_required] = original_override
 
 
 def test_register_returns_409_when_conflict(client: TestClient, mocker):
@@ -43,12 +68,12 @@ def test_register_returns_409_when_conflict(client: TestClient, mocker):
 
 def test_register_returns_registered_student_model(client: TestClient, mocker):
     mocker.patch('api.api_v1.routes.students.crud_user.exists', return_value=None)
-    mocker.patch('api.api_v1.routes.students.crud_user.create', return_value=new_student_response)
+    mocker.patch('api.api_v1.routes.students.crud_user.create', return_value=student_response)
 
     response = client.post('/students/register', json=new_student_request)
 
     assert response.status_code == status.HTTP_201_CREATED
-    assert response.json() == new_student_response
+    assert response.json() == student_response
 
 
 def test_update_profile_picture_returns_successful_msg(client: TestClient, mocker):
@@ -61,3 +86,24 @@ def test_update_profile_picture_returns_successful_msg(client: TestClient, mocke
 
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json() == 'Profile picture successfully uploaded!'
+
+
+def test_update_profile_picture_invalid_file(client: TestClient, mocker):
+    mocker.patch('api.api_v1.routes.students.crud_user.add_picture', return_value=False)
+
+    test_file = io.BytesIO(b"fake image data")
+    files = {'file': ('test_image.png', test_file, 'image/png')}
+
+    response = client.post('/students', files=files)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {'detail': 'File is corrupted or media type is not supported'}
+
+
+def test_view_account_returns_student_account_info(client: TestClient, mocker):
+    mocker.patch('api.api_v1.routes.students.crud_student.get_student', return_value=student_response)
+
+    response = client.get('/students')
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == student_response
