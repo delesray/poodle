784af82a46bd925956233f8f622e4433f4790a35
DenitsaTH <denizza.cristova@gmail.com>
2024-06-04 16:52:15 +0300

    students: more tests

--- src/app/tests/api/api_v1/endpoints/student_test.py
+++ src/app/tests/api/api_v1/endpoints/student_test.py
@@ -1,16 +1,13 @@
+import io
+import pytest
 from fastapi.testclient import TestClient
-from db.models import Account, Student
+from db.models import Account
 from core.security import Token
-
 
 account = Account(email='dummy@mail.com',
                   password='dummy pass',
                   role='student',
                   is_deactivated=False)
-
-student = Student(student_id=1,
-                  first_name='DummyFirstName',
-                  last_name='DummyLastName')
 
 new_student_request = {
     "email": "dummy@mail.com",
@@ -25,11 +22,17 @@ new_student_response = {
     "is_premium": False
 }
 
-
 token = Token(access_token='valid_token',
               token_type='bearer')
 
-def test_register_returns_409_when_conflict(client: TestClient, mocker):
+
+@pytest.fixture
+def mock_student():
+    student = account.student
+    return student
+
+
+def test_register_returns_409_when_conflict(client: TestClient, mocker, test_db, db):
     mocker.patch('api.api_v1.routes.students.crud_user.exists', return_value=account)
 
     response = client.post('/students/register', json=new_student_request)
@@ -38,7 +41,7 @@ def test_register_returns_409_when_conflict(client: TestClient, mocker):
     assert response.json() == {'detail': 'Email already registered'}
 
 
-def test_register_returns_registered_student_model(client: TestClient, mocker):
+def test_register_returns_registered_student_model(client: TestClient, mocker, test_db, db):
     mocker.patch('api.api_v1.routes.students.crud_user.exists', return_value=None)
     mocker.patch('api.api_v1.routes.students.crud_user.create', return_value=new_student_response)
 
@@ -47,6 +50,16 @@ def test_register_returns_registered_student_model(client: TestClient, mocker):
     assert response.status_code == 201
     assert response.json() == new_student_response
 
+# todo fix
+def test_update_profile_picture_returns_successful_msg(client: TestClient, mocker, test_db):
+    mocker.patch('api.api_v1.routes.students.crud_user.add_picture', return_value=True)
 
-def test_update_profile_picture_returns_successful_msg(client: TestClient, mocker):
-    pass
+    test_image_content = b'Test image content'
+    test_image = io.BytesIO(test_image_content)
+
+    response = client.post(f'/students',
+                           headers={"Authorization": f"Bearer {token}"},
+                           files={"image": ("test_image.jpg", test_image, "image/jpeg")})
+
+    assert response.status_code == 201
+    assert response.json() == "Profile picture successfully uploaded!"
