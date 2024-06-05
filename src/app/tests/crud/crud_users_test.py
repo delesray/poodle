46437615adab8fd0a447dba74ee2b125dc80d12b
miray-mustafov <mustafovmiray@gmail.com>
2024-06-05 23:34:40 +0300

    crud_users_test update:

--- src/app/tests/crud/crud_users_test.py
+++ src/app/tests/crud/crud_users_test.py
@@ -1,5 +1,4 @@
 from unittest.mock import Mock
-
 import pytest
 from fastapi import HTTPException, status
 from tests import dummies
@@ -42,7 +41,7 @@ async def test_get_specific_user_or_raise_404_happy_case(db, test_db):
 
 
 @pytest.mark.asyncio
-async def test_get_create_user_raises_409(db, test_db, mocker):
+async def test_get_create_user_raises_409(db, test_db):
     account, admin = await dummies.create_dummy_admin(db)
     mock_user = Mock()
     mock_user.email = dummies.dummy_user.email
@@ -53,3 +52,27 @@ async def test_get_create_user_raises_409(db, test_db, mocker):
         await crud_user.create_user(db=db, user=mock_user)
 
     assert e.value.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.asyncio
+async def test_get_create_user_happy_case(db, test_db):
+    mock_user = Mock()
+    mock_user.email = dummies.dummy_user.email
+    mock_user.password = dummies.dummy_user.password
+    mock_user.get_type = lambda: 'admin'
+
+    res_user = await crud_user.create_user(db=db, user=mock_user)
+
+    assert res_user.email == mock_user.email
+
+
+@pytest.mark.asyncio
+async def test_try_login_happy_case(db, test_db, mocker):
+    account, admin = await dummies.create_dummy_admin(db)
+    mocker.patch('crud.crud_user.hashing.verify_password', return_value=True)
+
+    res_user = await crud_user.try_login(
+        db=db, username=account.email, password=dummies.dummy_user.password
+    )
+
+    assert res_user.email == account.email
