53385f1e0af00832375c903d1f45ae460b81f057
miray-mustafov <mustafovmiray@gmail.com>
2024-06-07 14:33:04 +0300

    crud_user_test

--- src/app/core/settings.py
+++ src/app/core/settings.py
@@ -35,4 +35,3 @@ def get_settings() -> Settings:
 
 
 settings = get_settings()
-print(settings.DB_URL)
--- src/app/tests/crud/crud_users_test.py
+++ src/app/tests/crud/crud_users_test.py
@@ -5,14 +5,14 @@ from tests import dummies
 from crud import crud_user
 
 
-@pytest.mark.asyncio
-async def test_get_specific_user_or_raise_404_if_no_such_account(db):
-    with pytest.raises(HTTPException) as e:
-        await crud_user.get_specific_user_or_raise_404(
-            db=db, user_id=dummies.NON_EXISTING_ID, role='student'
-        )
-
-    assert e.value.status_code == status.HTTP_404_NOT_FOUND
+# @pytest.mark.asyncio
+# async def test_get_specific_user_or_raise_404_if_no_such_account(db):
+#     with pytest.raises(HTTPException) as e:
+#         await crud_user.get_specific_user_or_raise_404(
+#             db=db, user_id=dummies.NON_EXISTING_ID, role='student'
+#         )
+#
+#     assert e.value.status_code == status.HTTP_404_NOT_FOUND
 
 
 @pytest.mark.asyncio
@@ -27,31 +27,31 @@ async def test_get_specific_user_or_raise_404_if_no_match(db):
 
     assert e.value.status_code == status.HTTP_404_NOT_FOUND
 
+#
+# @pytest.mark.asyncio
+# async def test_get_specific_user_or_raise_404_happy_case(db):
+#     _, student = await dummies.create_dummy_student(db)
+#     account, teacher = await dummies.create_dummy_teacher(db)
+#
+#     res_teacher = await crud_user.get_specific_user_or_raise_404(
+#         db=db, user_id=account.account_id, role='teacher'
+#     )
+#
+#     assert res_teacher.teacher_id == teacher.teacher_id
+
 
 @pytest.mark.asyncio
-async def test_get_specific_user_or_raise_404_happy_case(db):
-    _, student = await dummies.create_dummy_student(db)
-    account, teacher = await dummies.create_dummy_teacher(db)
+async def test_create_user_raises_409(db):
+    account, admin = await dummies.create_dummy_admin(db)
+    mock_user = Mock()
+    mock_user.email = dummies.dummy_user.email
+    mock_user.password = dummies.dummy_user.password
+    mock_user.get_type = lambda: 'admin'
 
-    res_teacher = await crud_user.get_specific_user_or_raise_404(
-        db=db, user_id=account.account_id, role='teacher'
-    )
+    with pytest.raises(HTTPException) as e:
+        await crud_user.create_user(db=db, user=mock_user)
 
-    assert res_teacher.teacher_id == teacher.teacher_id
-
-
-# @pytest.mark.asyncio
-# async def test_create_user_raises_409(db):
-#     account, admin = await dummies.create_dummy_admin(db)
-#     mock_user = Mock()
-#     mock_user.email = dummies.dummy_user.email
-#     mock_user.password = dummies.dummy_user.password
-#     mock_user.get_type = lambda: 'admin'
-#
-#     with pytest.raises(HTTPException) as e:
-#         await crud_user.create_user(db=db, user=mock_user)
-#
-#     assert e.value.status_code == status.HTTP_409_CONFLICT
+    assert e.value.status_code == status.HTTP_409_CONFLICT
 
 # @pytest.mark.asyncio
 # async def test_get_create_user_happy_case(db, test_db):
